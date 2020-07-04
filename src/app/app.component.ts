0d0edb6db2c7f8e04a697d067a6ab435c313aac7
jnsnrz <evgenia.gq@gmail.com>
2020-07-04 19:48:42 +0300

    added todo-service and list of items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {TodoDataService} from '../services/todo-data.service';
+import {Todo} from '../models/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService],
 })
 export class AppComponent {
-  title = 'to-do-app';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+    this.todoDataService = todoDataService;
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodo(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleCompleteStatus(todo);
+  }
 }
--- src/services/todo-data.service.ts
+++ src/services/todo-data.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import {ITodo, Todo} from '../models/todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoDataService {
 
+  lastId = 0;
+  todos: Todo[] = [];
+
   constructor() { }
+
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  getTodoById(id: number): Todo {
+    return this.todos.filter(item => item.id === id).pop();
+  }
+
+  addTodo(item: Todo): TodoDataService {
+    if (!item.id) {
+      item.id = ++this.lastId;
+    }
+    this.todos.push(item);
+    return this;
+  }
+
+  updateTodo(id: number, values: {}): Todo {
+    const todo = this.getTodoById(id);
+
+    if (!todo) {
+      return null;
+    }
+
+    Object.assign(todo, values);
+
+    return todo;
+  }
+
+  deleteTodo(id: number): TodoDataService {
+    this.todos = this.todos.filter(item => item.id !== id);
+    return this;
+  }
+
+  toggleCompleteStatus(todo: Todo): Todo {
+    return this.updateTodo(todo.id, {complete: !todo.complete});
+  }
 }
